3067af1d96ae30f7eca6387b2420ba6a88a5ccb5
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-10-29 16:28:23 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES
+(
+    "Mr. Chocolate",4,'F','black','angry', True
+),
+(
+    "Rowdy",4,'F','black','angry', True
+),
+(
+    "Tabitha",4,'F','black','angry', True
+),
+(
+    "Sergeant Brown",4,'F','black','angry', True
+),
+(
+    "Melissa",4,'F','black','angry', True
+),
+(
+    "Grinch",4,'F','black','angry', True
+),
+(
+    "Wendy",4,'F','black','angry', True
+),
+(
+    NULL,4,'F','black','angry', True
+)
